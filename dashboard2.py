0f3a53504a0c56db80282fd7449715268fe0c7a8
Friedrich Finken <66962420+fritz-lewagon@users.noreply.github.com>
2022-05-17 20:04:48 +0200

    new style & cleaning elements

--- dashboard2.py
+++ dashboard2.py
@@ -14,18 +14,25 @@ external_stylesheets = [
 ]
 
 
-
+#import data frame
 df = pd.read_csv("data/final_list.csv")
 
-df.rename(columns={"Unnamed: 0": "Index", ""}, inplace = TRUE)
+
+# data cleaning - renaming
+df.rename(columns={"Unnamed: 0": "Index", "comp_name": "Company", "description": "Description",
+"business_model": "Business Model", "customer": "Target Customer", "keywords": "Keywords",
+"stage": "Stage", "total_funding": "Total Funding (in M€)", "num_investors": "Number of Investors",
+"date_founded": "Year Founded", "location": "Location", "employees": "Employees", "website": "website"}, 
+inplace = TRUE)
 
 
+# dash starts here
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     children=[
         #header part
-        html.Img(src=("/assets/Logo.png")),
+        html.Img(src=("/assets/Logo.png"),className="logo-title"),
         html.H1(children="Start Up List",className="header-title",),
         html.P(children="Last scrape was the 12.12.12",className="header-description",),
         
